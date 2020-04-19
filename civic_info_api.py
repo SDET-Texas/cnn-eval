82c6c458958a4ea96264c117177affd90223905f
SDET-Texas <61237103+SDET-Texas@users.noreply.github.com>
2020-04-19 12:33:21 -0500

    Added Tests and verifications

--- civic_info_api.py
+++ civic_info_api.py
@@ -7,13 +7,19 @@ class CivicInfo:
     def __init__(self):
         self.url = None
         stream = open('civic_info.yaml', 'r')
-        dictionary = yaml.load(stream)
+        dictionary = yaml.load(stream, Loader=yaml.SafeLoader)
         self.url = dictionary['url']
         self.api_key = dictionary['api_key']
 
-    def elections_query(self, identifier=None, name=None, election_day=None, ocdDivisionId=None):
-        elections_url = self.url + r'/elections'
-        result = requests.get(elections_url + r'?key=' + self.api_key)
+    def elections_query(self):
+        """
+        :return: A json response listing available elections.
+        The following fields appear in result:
+        id, name, election_day, ocd_division_id
+        Note:  This appears to be still under development.  Filtering appears to be not available
+        """
+        elections_url = self.url + r'/elections/?key=' + self.api_key
+        result = requests.get(elections_url)
         return result
 
     def voter_query(self, address, election_id, return_all_available_data=True, official_only=None):
@@ -21,22 +27,22 @@ class CivicInfo:
         request_string = voter_url + r'?key=' + \
             self.api_key + r'&address=' + address + r'&electionId=' + str(election_id)
         result = requests.get(request_string)
-        return result
+        raise NotImplementedError
 
     def rep_info_by_address(self, levels_filter=None):
         rep_address = self.url + r'/representatives'
         request_string = rep_address
         result = requests.get(request_string)
-        return result
+        raise NotImplementedError
 
     def rep_info_by_division(self, ocd_id):
         rep_division = self.url + r'/representatives/' + ocd_id
         request_string = rep_division
         result = requests.get(request_string)
-        return result
+        raise NotImplementedError
 
     def search_divisions(self, query):
         search_url = self.url + r'/divisions'
         request_string = search_url
         result = requests.get(request_string)
-        return result
+        raise NotImplementedError
--- civic_info_tests.py
+++ civic_info_tests.py
@@ -1,13 +1,46 @@
+import logging
 import yaml
 import civic_info_api
 
-stream = open('civic_info.yaml', 'r')
-dictionary = yaml.load(stream)
+logger = logging.getLogger('civic_info_test')
+fh = logging.FileHandler('civic_info_test.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+logger.setLevel(logging.INFO)
 
-civic_info = civic_info_api.CivicInfo()
 
-election_info = civic_info.elections_query()
+class CivicInfoTests:
 
-voter_info = civic_info.voter_query(dictionary['test_address'], dictionary['test_election'])
+    def __init__(self):
+        stream = open('civic_info.yaml', 'r')
+        self.dictionary = yaml.load(stream, Loader=yaml.SafeLoader)
+        self.civic_info = civic_info_api.CivicInfo()
 
-print(civic_info)
+    def test_election_query(self):
+        election_info = self.civic_info.elections_query()
+        election_info_content = election_info.content.decode('utf-8')
+        expected_names = self.dictionary['election_test_results']
+        for name in expected_names:
+            logger.info('verifying for ' + name)
+            if name not in election_info_content:
+                logger.warning('Election query test FAILED.')
+                return False
+
+        logger.info('Election query test passed.')
+        return True
+
+    def test_voter_query(self):
+        voter_info = self.civic_info.voter_query(self.dictionary['test_address'],
+                                                 self.dictionary['test_election'])
+
+
+def main():
+    civic_info_tests = CivicInfoTests()
+    result = civic_info_tests.test_election_query()
+    print(result)
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
